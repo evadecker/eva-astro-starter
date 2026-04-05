6ebf883dbf7355c50ed7bfb8291eaab1666d03ff
Ky Decker <itsevadecker@gmail.com>
2026-04-05 17:38:46 -0400

    Remove Stylelint and add lint-staged (#97)
    * Remove Stylelint and add lint-staged
    
    * Update name and add author to package.json

--- stylelint.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('stylelint').Config} */
-export default {
-  extends: ["stylelint-config-standard", "stylelint-config-astro"],
-  ignoreFiles: ["dist/**/*"],
-  rules: {
-    // Add your own rules here
-  },
-};
